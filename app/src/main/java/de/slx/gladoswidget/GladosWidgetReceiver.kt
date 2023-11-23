89ee6d7b2504815e8ab5c2e9a5d5159aa4fd22ea
Slx-hub <schneja@tuta.io>
2023-11-23 17:06:26 +0100

    clean Project setup

--- app/src/main/java/de/slx/gladoswidget/GladosWidgetReceiver.kt
+++
@@ -1,9 +0,0 @@
-package de.slx.gladoswidget
-
-import androidx.glance.appwidget.GlanceAppWidget
-import androidx.glance.appwidget.GlanceAppWidgetReceiver
-
-class GladosWidgetReceiver : GlanceAppWidgetReceiver() {
-    // Let MyAppWidgetReceiver know which GlanceAppWidget to use
-    override val glanceAppWidget: GlanceAppWidget = GladosWidget()
-}
